7bc17e0c86adc0d1b5eb51d717824b600bf8aa26
azza999 <azzadh999@gmail.com>
2021-04-22 15:42:17 +0900

    finish chrome-extension getting started

--- pj-chrome-extension/chrome-extension/background.js
+++ pj-chrome-extension/chrome-extension/background.js
@@ -1,6 +1,6 @@
 let color = '#0ff';
 
-// chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
+});
