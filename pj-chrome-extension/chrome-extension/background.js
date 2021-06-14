009ea98e4a7933b4a876b4cd69ec9ed4abc823c6
azzadh999@gmail.com <azzadh999@gmail.com>
2021-06-14 23:44:34 +0900

    add chrome package

--- pj-chrome-extension/chrome-extension/background.js
+++ pj-chrome-extension/chrome-extension/background.js
@@ -1,6 +1,9 @@
-let color = '#0ff';
+let ver2hor = true;
+let hor2ver = true;
+let floatOption = false;
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
+  chrome.storage.sync.set({ ver2hor });
+  chrome.storage.sync.set({ hor2ver });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
