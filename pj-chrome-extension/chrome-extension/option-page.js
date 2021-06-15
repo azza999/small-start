a7ac1b947ccf561cca752d1bf6640c9145597742
azzadh999@gmail.com <azzadh999@gmail.com>
2021-06-15 20:15:15 +0900

    add chrome-extension

--- pj-chrome-extension/chrome-extension/option-page.js
+++ pj-chrome-extension/chrome-extension/option-page.js
@@ -4,35 +4,11 @@ const floatOption = document.getElementById('float-option');
 
 // Add a button to the page for each supplied color
 function constructOptions() {
-	/*
-	chrome.storage.sync.get("color", (data) => {
-		let currentColor = data.color;
-
-		// For each color we were provided…
-		for (let buttonColor of buttonColors) {
-			// …crate a button with that color…
-			let button = document.createElement("button");
-			button.dataset.color = buttonColor;
-			button.style.backgroundColor = buttonColor;
-
-			// …mark the currently selected color…
-			if (buttonColor === currentColor) {
-				button.classList.add(selectedClassName);
-			}
-
-			// …and register a listener for when that button is clicked
-			button.addEventListener("click", handleButtonClick);
-			page.appendChild(button);
-		}
-	});
-	*/
 
 	chrome.storage.sync.get("ver2horState", (data) => {
-		console.log(data)
 		let ver2horState = data.ver2horState
 		ver2hor.checked = ver2horState
 		ver2hor.addEventListener("click", e=>{
-			console.log(e)
 			ver2horState = ver2hor.checked
 			chrome.storage.sync.set({ ver2horState })
 		})
--- pj-chrome-extension/chrome-extension/popup.js
+++ pj-chrome-extension/chrome-extension/popup.js
@@ -1,29 +1,56 @@
-/*
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+const applyBtn = document.getElementById('apply');
+let status = null;
 
-chrome.storage.sync.get("color", ({ color }) => {
-	changeColor.style.backgroundColor = color;
-});
+function applyOption() {
+	// 영화관모드인지, 일반 모드인지 체크
+	let isTheaterMode = document.querySelector('#player-theater-container').childElementCount
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+	// 동영상 형태 확인 (가로세로)
+	let video = document.querySelector('video')
+	let rect = video.getClientRects()[0]
+	
+	let videoShape = rect.height > rect.width ? 'ver' : 'hor';
+
+	document.body.append(videoShape)
+
+	// 일반 모드일때
+	if ( !isTheaterMode ) {
+
+		if (videoShape === 'hor') {
+
+			let left = video.offsetHeight + (video.offsetWidth - video.offsetHeight) / 2
+
+			document.querySelectorAll('#player')[1].style.height = video.offsetWidth + 'px'
+			document.getElementById('player-container').style.height = video.offsetWidth + 'px'
+
+			video.style.transformOrigin = "left top";
+			video.style.transform = `rotateZ(90deg) translate(0, -${left}px)`;
+			console.log(`rotateZ(90deg) translate(0, -${left}px)`)
+
+			try {
+				document.querySelector('.ytp-tooltip').style.transform = `translate(200px, 0)`
+			} catch (e) {}
+		}
+	}
+
+}
+
+async function applyTab() {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
-		function: setPageBackgroundColor,
-	});
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-
-	chrome.storage.sync.get("color", ({ color }) => {
-		document.body.style.backgroundColor = color;
-	});
-	
+		function: applyOption
+	})
 }
-*/
 
+applyBtn.addEventListener("click", applyTab)
+
+chrome.storage.sync.get(["ver2horState", "hor2verState", "floatOptionState"], (data) => {
+	
+	// 디버깅
+	document.body.append(JSON.stringify(data))
+	status = data
+
+	// 최초 1회 실행
+	applyTab()
+})
